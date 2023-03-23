6b5750a01ea13809cc2e4a74621d8d7a8717335f
Mu-il Yang <muilyang12@gmail.com>
2023-03-23 23:39:43 +0900

    10 예제 webpack config 수정.

--- 10.Bowling/webpack.config.js
+++ 10.Bowling/webpack.config.js
@@ -6,6 +6,23 @@ module.exports = {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.glb$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, "src"),
